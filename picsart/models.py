345ee4fced504aab42b163e9b1b8151b4637cec0
victor njuguna <victormoore254@gmail.com>
2018-06-08 12:50:23 +0300

    this commit creates the location, category and image models

--- picsart/models.py
+++ picsart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+
+class Location(models.Model):
+    image_location = models.CharField(max_length = 30)
+
+
+class Category(models.Model):
+    image_category = models.CharField(max_length = 30)
+
+
+class Image(models.Model):
+    name = models.CharField(max_length = 30)
+    image = models.ImageField(upload_to = 'pics/', default = 'image')
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+
+
+    
+    
