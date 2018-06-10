3d0bc373384e99053047b4297b425f7173a22026
victor njuguna <victormoore254@gmail.com>
2018-06-10 19:44:08 +0300

    this commit tests saving in all three models

--- picsart/tests.py
+++ picsart/tests.py
@@ -12,19 +12,34 @@ class LocationTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.test_location, Location))
 
+    # Testing saving
+    def test_save_method(self):
+        self.test_location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
 
 class CategoryTestClass(TestCase):
     def setUp(self):
-        self.test = Category(image_category = "Food")
+        self.category = Category(image_category = "Food")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.test, Category))
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_saving_category(self):
+        self.category.save_category()
+        categorys = Category.objects.all()
+        self.assertTrue(len(categorys) > 0)
 
 
 class ImageTestClass(TestCase):
     def setUp(self):
-        # Location
         self.image = Image(name = 'Sunset', description = 'Beach sunset in Thailand')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.image, Image))
+
+    def test_saving_image(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
