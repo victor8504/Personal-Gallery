d655669b6a1e4e92974c7db74952af5b5cc35cb2
victor njuguna <victormoore254@gmail.com>
2018-06-10 15:45:23 +0300

    this commit create the update class method for all models and alters thr image and location model schema

--- picsart/models.py
+++ picsart/models.py
@@ -4,10 +4,11 @@ import datetime as dt
 # Create your models here.
 
 class Location(models.Model):
-    image_location = models.CharField(max_length = 30)
+    NAIROBI = 'NAIROBI'
+    MOMBASA = 'MOMBASA'
+    JUJA = 'JUJA'
 
-    def __str__(self):
-        return self.image_location
+    location = models.CharField(max_length = 30)
 
     def save_location(self):
         self.save()
@@ -15,19 +16,32 @@ class Location(models.Model):
     def delete_location(self):
         self.delete()
 
+    @classmethod
+    def update_location(cls, id, location, update):
+        updated = cls.objects.filter(id = id).update(location = update)
+        return updated
+
+    def __str__(self):
+        return self.location
+
 
 
 class Category(models.Model):
     image_category = models.CharField(max_length = 30)
         
-    def __str__(self):
-        return self.image_category
-
     def save_category(self):
         self.save()
 
     def delete_category(self):
         self.delete()
+
+    @classmethod
+    def update_category(cls, id, category, update):
+        updated = cls.objects.filter(id = id).update(category = update)
+        return updated
+
+    def __str__(self):
+        return self.image_category  
 
 
 
@@ -35,9 +49,13 @@ class Image(models.Model):
     name = models.CharField(max_length = 30)
     image = models.ImageField(upload_to = 'pics/', default = 'image')
     description = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add = True)
+    image_url = models.TextField(blank = True)
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
+
+    class Meta:
+        ordering = ['pub_date']
 
     def __str__(self):
         return self.name
@@ -47,6 +65,11 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+
+    @classmethod
+    def update_image(cls, id, image, update):
+        updated = cls.objects.filter(id = id).update(image = update)
+        return updated
 
 
     
