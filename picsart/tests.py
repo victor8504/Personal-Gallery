060508acfd2217089cc899ed22b6d852a68c0085
victor njuguna <victormoore254@gmail.com>
2018-06-10 19:26:56 +0300

    this commit tests instance for all three models

--- picsart/tests.py
+++ picsart/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.test_location = Location(location = "Nairobi")
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_location, Location))
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.test = Category(image_category = "Food")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test, Category))
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        # Location
+        self.image = Image(name = 'Sunset', description = 'Beach sunset in Thailand')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
