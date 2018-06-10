6f37ecce9f2d61514e65e52034dc48b16bbc37d9
victor njuguna <victormoore254@gmail.com>
2018-06-10 23:53:53 +0300

    this commit creates the search results html file and its respective view function and url

--- picsart/urls.py
+++ picsart/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    image('image/<int:image_id>', views.image, name = 'single_image'),
+    url('search', views.search_results, name='search_results'),
+    url('image/<int:image_id>', views.image, name = 'single_image'),
 ]
 
 if settings.DEBUG:
--- picsart/views.py
+++ picsart/views.py
@@ -9,3 +9,15 @@ def index(request):
 def image(request, image_id):
     image = Image.get_image(image_id)
     return render(request, 'image.html', {"image": image})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        query = request.GET.get("image")
+        results = Image.searched(query)
+        message = f"{query}"
+
+        return render(request, 'results.html',{"message": message, "results": results})
+    else:
+        message = "What images are you interested in?"
+        return render(request, 'results.html',{"message": message})
